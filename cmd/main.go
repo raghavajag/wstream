88f911bf53a02073ef5d5cead64a3c599fb864a3
raghavajag <raghavajagarwal@gmail.com>
2024-11-04 15:39:51 +0530

    feat: setting up config env

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backend_task/internal/config"
 	"log"
 	"net/http"
 
@@ -9,14 +10,17 @@ import (
 
 func main() {
 	router := gin.Default()
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
 
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "ok",
 		})
 	})
-
-	port := ":8080"
+	port := cfg.Server.Port
 	log.Printf("Starting server on port %s", port)
 	if err := router.Run(port); err != nil {
 		log.Fatal(err)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,46 @@
 package config
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server struct {
+		Port    string
+		BaseURL string
+	}
+}
+
+// retrieves an environment variable with a default value
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultValue int) int {
+	valueStr := os.Getenv(key)
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultValue
+}
+
+func Load() (*Config, error) {
+	// Load .env file if it exists
+	godotenv.Load()
+
+	config := &Config{}
+
+	// Server configuration
+	config.Server.Port = getEnv("SERVER_PORT", ":8080")
+	config.Server.BaseURL = getEnv("BASE_URL", "http://localhost:8080")
+
+	// Audio configuration
+
+	return config, nil
+}
