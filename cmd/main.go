db08c2089d77688b945ea044578c9ec052cb0dec
raghavajag <raghavajagarwal@gmail.com>
2024-11-04 15:49:25 +0530

    feat: custom app module

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,20 @@
 package main
 
 import (
+	"backend_task/internal/app"
 	"backend_task/internal/config"
 	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-		})
-	})
-	port := cfg.Server.Port
-	log.Printf("Starting server on port %s", port)
-	if err := router.Run(port); err != nil {
-		log.Fatal(err)
+	app := app.New(cfg)
+	if err := app.Run(); err != nil {
+		log.Fatalf("Failed to run application: %v", err)
 	}
+
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,30 @@
 package app
+
+import (
+	"backend_task/internal/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	config *config.Config
+	router *gin.Engine
+}
+
+func New(cfg *config.Config) *App {
+	return &App{
+		config: cfg,
+		router: gin.Default(),
+	}
+}
+
+func (a *App) Run() error {
+	a.setupRoutes()
+	return a.router.Run(a.config.Server.Port)
+}
+
+func (a *App) setupRoutes() {
+	a.router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+}
